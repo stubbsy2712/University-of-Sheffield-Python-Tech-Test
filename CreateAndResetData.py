e2a07bae6c4804aabe934b65beb5701d3267e95c
Thomas Stubbs <stubbsy2712@gmail.com>
2026-03-16 18:48:52 +0000

    imported sql file used for db, and changed the python file to just run that

--- CreateAndResetData.py
+++ CreateAndResetData.py
@@ -1,19 +1,16 @@
 import sqlite3
-connection = sqlite3.connect("db.sqlite")
-cursor = connection.cursor()
+import os
 
-def create_data():
-	# Code to create data goes here
-	print("Data created successfully.")
-
-def cleanup_data():
-	cursor.execute("`id`, `first_name`, `surname`, `email`, `status` ENUM('archived','active','suspended')")
-	cursor.execute("""
-    INSERT INTO `customer` VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)""")
-	print("Data cleaned up successfully.")
+def execute_sql():
+	db_path = "./db.sqlite"
+	sql_file = "./SetupAndAddTestData.sql"
+	conn = sqlite3.connect(db_path)
+	cursor = conn.cursor()
+	if os.path.exists(sql_file):
+		with open(sql_file, 'r') as f:
+			sql_script = f.read()
+			cursor.executescript(sql_script)
+			conn.commit()
 
 if __name__ == "__main__":
-	cleanup_data()
-	create_data()
+    execute_sql()
